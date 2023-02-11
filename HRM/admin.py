625e44840c0dc7ca9a7708c1874f124400b5dc78
BiloungaReine <rbilounga0@gmail.com>
2023-02-11 15:45:31 +0100

    creation of superadmin reine

--- HRM/admin.py
+++ HRM/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Department
+from .models import Employee
+from .models import Documents
+from .models import Agent
+from .models import Contract
+
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Documents)
+admin.site.register(Agent)
+admin.site.register(Contract)
+
--- HRM/models.py
+++ HRM/models.py
@@ -5,8 +5,7 @@ from django.db import models
 
 class Department(models.Model):
    departmentId = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
-   Employee = models.ManyToOneRel(Employee, blank=True, null=True, on_delete=models.CASCADE)
-   subDepartement = models.ForeignKey("sub departement","self",related_name="sub_departement", on_delete=models.CASCADE,null=True, blank=True)
+   #subDepartement = models.ForeignKey('sub departement',"self",related_name="sub_departement", on_delete=models.CASCADE, null=True, blank=True)
    dept_name = models.CharField(max_length=255, blank=True, null=True)
    description = models.CharField(max_length=255, blank=True, null=True)
    deptnumber = models.CharField(max_length=255, blank=True, null=True)
@@ -18,15 +17,16 @@ class Department(models.Model):
 
 
 class Employee(models.Model):
+    TYPE_CHOICES =(('celibat','Célibataire'),('marie', 'Marié(e)'), ('divorce', 'Divorcé(e)'), ('veuf ','Veuf(ve)'), ('concubin', 'Concubinage'))
     employeeId = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
     department = models.ForeignKey(Department, blank=True, null=True, on_delete=models.CASCADE)
-    subEmployee = models.ForeignKey("sub Employee","self",related_name="sub_Employee", on_delete=models.CASCADE,null=True, blank=True)
+    #subEmployee = models.ForeignKey("sub Employee","self",related_name="sub_Employee", on_delete=models.CASCADE,null=True, blank=True)
     firstname = models.CharField(max_length=255, blank=True, null=True)
     lastname = models.CharField(max_length=255, blank=True, null=True)
     birthdate = models.DateField(max_length=10, blank=True, null=True)
     birthplace = models.CharField(max_length=255, blank=True, null=True)
     gender = models.CharField(max_length=20,blank=True, null=True)
-    status = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True, choices=TYPE_CHOICES, default='celibat')
     email = models.CharField(max_length=255, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=10, blank=True, null=True)
@@ -46,20 +46,20 @@ class Employee(models.Model):
     def __str__(self):
         return str(self.firstname)
 
-    class Meta:
-        abstract = True
+    #class Meta:
+        #abstract = True
 
-class Manager(Employee):
-    manage_empl = models.CharField(max_length=255)
+ #class Manager(Employee):
+     #manage_empl = models.CharField(max_length=255)
 
-class Supervisor(Employee):  
-    supervise_work = models.CharField(max_length=255)  
+ #class Supervisor(Employee):  
+     #supervise_work = models.CharField(max_length=255)  
 
-class Accountant(Employee):
-    pay_empl = models.CharField(max_length=255)
+ #class Accountant(Employee):
+     #pay_empl = models.CharField(max_length=255)
 
-class Worker(Employee):
-    firm_work = models.CharField(max_length=255)
+ #class Worker(Employee):
+     #firm_work = models.CharField(max_length=255)
 
 
 class Documents(models.Model):
@@ -94,15 +94,15 @@ class Agent(models.Model):
     def __str__(self):
         return str(self.firstname)   
 
-    class Meta:
-        abstract = True
+    #class Meta:
+        #abstract = True
 
-class Client(Agent):
-    paydate = models.DateField(max_length=255)
+ #class Client(Agent):
+     #paydate = models.DateField(max_length=255)
     
-class Supplier(Agent):  
-    deliverydate = models.DateField(max_length=255) 
-                 
+ #class Supplier(Agent):  
+     #deliverydate = models.DateField(max_length=255) 
+
 
 class Contract(models.Model):
     contractId = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
@@ -112,24 +112,24 @@ class Contract(models.Model):
     dateofcreation = models.DateField(max_length=255, blank=True, null=True)
     duration = models.DateField(max_length=255, blank=True, null=True)
     formofcontract = models.CharField(max_length=255, blank=True, null=True)
-    contractupload = models.FieldFile(upload_to="ContractFile/contract.pdf", blank=True, null=True)
+    contractupload = models.FileField(upload_to="ContractFile/contract.pdf", blank=True, null=True)
     createDate = models.DateTimeField(auto_now_add=True)
     updateDate = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.contract_no)
 
-    class Meta:
-        abstract = True
+    #class Meta:
+        #abstract = True
 
-class Employee_cont(Contract):
-    paydate = models.DateField(max_length=255)
+ #class Employee_cont(Contract):
+     #paydate = models.DateField(max_length=255)
 
-class Supplier_cont(Contract):  
-    deliverydate = models.DateField(max_length=255)  
+ #class Supplier_cont(Contract):  
+     #deliverydate = models.DateField(max_length=255)  
 
-class Client_cont(Contract):
-    paydate = models.DateField(max_length=255)
+ #class Client_cont(Contract):
+     #paydate = models.DateField(max_length=255)
 
 
 
