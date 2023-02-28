d622ccb049e48c2ca5e10e4fefc54ca6c9e08405
popifra1409 <porio.peyie@gmail.com>
2023-02-28 09:30:41 +0100

    serializers modified

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -1,27 +1,52 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Agent, Contract, Department, Employee, Document
+from .models import Client, Supplier, Internal, External, Department, Supervisor, Manager, Accountant,Worker, Document
 
 class DepartmentSerializer(ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
 
-class EmployeeSerializer(ModelSerializer):
+class SupervisorSerializer(ModelSerializer):
     class Meta:
-        model = Employee
-        fields = '__all__'        
+        model = Supervisor
+        fields = '__all__'       
+
+class ManagerSerializer(ModelSerializer):
+    class Meta:
+        model = Manager
+        fields = '__all__'   
+
+class AccountantSerializer(ModelSerializer):
+    class Meta:
+        model = Accountant
+        fields = '__all__' 
+
+class WorkerSerializer(ModelSerializer):
+    class Meta:
+        model = Worker
+        fields = '__all__'            
 
 class DocumentSerializer(ModelSerializer):
     class Meta:
         model = Document
         fields = '__all__'                
 
-class ContractSerializer(ModelSerializer):
+class InternalSerializer(ModelSerializer):
     class Meta:
-        model = Contract
-        fields = '__all__'                          
+        model = Internal
+        fields = '__all__'       
 
-class AgentSerializer(ModelSerializer):
+class ExternalSerializer(ModelSerializer):
     class Meta:
-        model = Agent
-        fields = '__all__'                                        
+        model = External
+        fields = '__all__'                     
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'      
+
+class SupplierSerializer(ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'                                      
